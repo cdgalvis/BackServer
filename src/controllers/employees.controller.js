457c8c1cf712ecd7d7a5eced68d4c379b9be4296
cdgalvis <cristiangalviscardona@gmail.com>
2022-02-09 20:12:04 -0500

    Fase inicial backend

---
+++ src/controllers/employees.controller.js
@@ -0,0 +1,10 @@
+const employeeCtrl = {}
+
+employeeCtrl.getEmployees = (req, res) => {}
+employeeCtrl.createEmployee = (req, res) => {}
+employeeCtrl.getEmployee = (req, res) => {}
+employeeCtrl.editEmployee = (req, res) => {}
+employeeCtrl.deleteEmployee = (req, res) => {}
+
+
+module.exports = employeeCtrl
