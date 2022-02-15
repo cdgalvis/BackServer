a3dc95eda1cf15094f48f8f00ff658f9b61bac3a
cdgalvis <cristiangalviscardona@gmail.com>
2022-02-14 20:31:14 -0500

    Final Back

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -1,10 +1,32 @@
 const employeeCtrl = {}
+const Employee = require('../models/Employee')
 
-employeeCtrl.getEmployees = (req, res) => {}
-employeeCtrl.createEmployee = (req, res) => {}
-employeeCtrl.getEmployee = (req, res) => {}
-employeeCtrl.editEmployee = (req, res) => {}
-employeeCtrl.deleteEmployee = (req, res) => {}
+employeeCtrl.getEmployees = async (req, res) => {
+   const employees = await Employee.find()
+   res.json(employees)
+}
+
+employeeCtrl.createEmployee = async (req, res) => {
+    const newEmployee = new Employee(req.body)
+    await newEmployee.save()
+
+    res.send({ message: 'Employee Created' })
+}
+
+employeeCtrl.getEmployee = async (req, res) => {
+    const employee = await Employee.findById(req.params.id)
+    res.send(employee)
+}
+
+employeeCtrl.editEmployee = async (req, res) => {
+    await Employee.findByIdAndUpdate(req.params.id, req.body)
+    res.json({ status: 'Employee Update'})
+}
+
+employeeCtrl.deleteEmployee = async (req, res) => {
+    await Employee.findByIdAndDelete(req.params.id)
+    res.json({ status: 'Employee Deleted'})
+}
 
 
 module.exports = employeeCtrl
